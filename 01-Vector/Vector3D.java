5b83ad3627ef49e8c9ab86362b84ce84a7d352e8
Steven Liunardi <steven.liunardi@s1.informatika.unsyiah.ac.id>
2015-10-30 17:17:43 +0700

    vektor3d - ke2

--- 01-Vector/Vector3D.java
+++ 01-Vector/Vector3D.java
@@ -1,12 +1,22 @@
 
 public class Vector3D{
     
-    public double x;
-    public double y;
-    public double z;
+    private double x;
+    private double y;
+    private double z;
     
     public Vector3D(double x, double y, double z){
         this.x=x;
         this.y=y;
         this.z=z;
-    }}
+    }
+    
+    public double getX(){
+    return x;}
+    
+    public double getY(){
+    return y;}
+    
+    public double getZ(){
+    return z;}
+}
