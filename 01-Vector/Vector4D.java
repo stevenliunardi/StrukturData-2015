67256d96970ec6f8f3ff2f75ef39d513c48f2212
Steven Liunardi <steven.liunardi@s1.informatika.unsyiah.ac.id>
2015-10-30 17:47:49 +0700

    Vector 4D - ke 1

---
+++ 01-Vector/Vector4D.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+
+
+public class Vector4D extends Vector3D {
+    private Date t;
+    
+    public Vector4D(double x, double y, double z, Date t){
+    
+    super (x,y,z);
+        this.t=t;
+    }
+    
+   public Date getT(){
+        return t;}
+    }
